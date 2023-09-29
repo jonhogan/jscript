f3bca832bcfb3e3f883953b04c5086ad9d2baa66
Jon <jon.hogan83@gmail.com>
2023-09-28 22:06:51 -0500

    Initial push

---
+++ complete-javascript-course/helloWorld/helloWorld.js
@@ -0,0 +1,9 @@
+//This is the console version
+
+alert("Hello world");
+
+js = "amazing";
+if (js === "amazing") alert("Javascript is fun");
+
+js = "boring";
+if (js === "amazing") alert("Javascript is fun");
